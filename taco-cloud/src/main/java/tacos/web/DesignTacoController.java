857e1cb6db96407c93031165f8db4a640aeaf49b
Rodrigo Girckus <rodrigogirckus@gmail.com>
2022-08-11 00:24:13 -0300

    Validação dos formulários

--- taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -3,8 +3,11 @@ package tacos.web;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,7 +30,11 @@ public class DesignTacoController {
 	}
 	
 	@PostMapping
-	public String processTaco(Taco taco, @ModelAttribute(name = "order") TacoOrder order) {
+	public String processTaco(@Valid @ModelAttribute(name = "design") Taco taco, Errors errors, @ModelAttribute(name = "order") TacoOrder order) {
+		if (errors.hasErrors()) {
+			return "design";
+		}
+		
 		order.addTaco(taco);
 		return "redirect:/orders/current";
 	}
